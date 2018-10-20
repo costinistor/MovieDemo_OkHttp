3bd8973bb686adcb15e39e731e9215be00ae10d8
C-Tin Nistor <kobo.turc@gmail.com>
2018-10-20 20:09:02 +0300

    some changes

--- app/src/main/java/com/moviedemo_okhttp/activity/MovieActivity.kt
+++ app/src/main/java/com/moviedemo_okhttp/activity/MovieActivity.kt
@@ -1,20 +1,13 @@
 package com.moviedemo_okhttp.activity
 
 import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
-import android.support.annotation.RequiresApi
 import android.support.constraint.ConstraintLayout
 import android.support.v7.app.AppCompatActivity
-import android.text.Layout
 import android.util.TypedValue
-import android.view.Gravity
-import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.TextView
-import com.moviedemo_okhttp.R
-import kotlinx.android.synthetic.main.empty_layout.*
+
 
 class MovieActivity : AppCompatActivity() {
 
@@ -32,7 +25,6 @@ class MovieActivity : AppCompatActivity() {
     }
 
     fun mainContainer(lv: ConstraintLayout){
-        //val lv = LinearLayout(this)
         lv.setBackgroundColor(Color.RED)
 
         val title = TextView(this)
@@ -40,11 +32,8 @@ class MovieActivity : AppCompatActivity() {
         title.setTextColor(Color.WHITE)
         title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 26f)
         val param = ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-        param.circleRadius= 60
 
         title.layoutParams = param
-
-        //title.textAlignment = View.TEXT_ALIGNMENT_CENTER
 
         lv.addView(title)
     }
